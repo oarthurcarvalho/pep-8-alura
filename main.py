2c55e9ce3aecfd2e390d5570ef4453f1bf38e4f8
Arthur Carvalho <arthur.reis@live.com>
2021-08-21 14:43:50 -0300

    criando arquivo fabrica_fila.py

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from fila_normal import FilaNormal
-from fila_prioritaria import FilaPrioritaria
+# from fila_normal import FilaNormal
+# from fila_prioritaria import FilaPrioritaria
+from fabrica_fila import FabricaFila
 
-fila_teste = FilaNormal()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-fila_teste.atualiza_fila()
-print(fila_teste.chama_cliente(5))
-print(fila_teste.chama_cliente(10))
+# fila_teste = FilaNormal()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# fila_teste.atualiza_fila()
+# print(fila_teste.chama_cliente(5))
+# print(fila_teste.chama_cliente(10))
 
 # fila_teste_2 = FilaPrioritaria()
 # fila_teste_2.atualiza_fila()
@@ -15,3 +16,9 @@ print(fila_teste.chama_cliente(10))
 # print(fila_teste_2.chama_cliente(10))
 # print(fila_teste_2.chama_cliente(1))
 # print(fila_teste_2.estatistica('10/01/1993', 216, 'detail'))
+
+teste_fabrica = FabricaFila.pega_fila(1)
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+teste_fabrica.atualiza_fila()
+print(teste_fabrica.chama_cliente(10))
