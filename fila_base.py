08e9e134baa7eb6347e5adc276e1644b86377e18
Arthur Carvalho <arthur.reis@live.com>
2021-08-21 14:30:31 -0300

    criando arquivo constantes.py

--- fila_base.py
+++ fila_base.py
@@ -1,5 +1,7 @@
 import abc
 
+from constantes import TAMANHO_PADRAO_MAXIMO, TAMANHO_PADRAO_MINIMO
+
 
 class FilaBase(metaclass=abc.ABCMeta):
 
@@ -9,8 +11,8 @@ class FilaBase(metaclass=abc.ABCMeta):
     senha_atual: str = ""
 
     def reseta_fila(self) -> None:
-        if self.codigo >= 200:
-            self.codigo
+        if self.codigo >= TAMANHO_PADRAO_MAXIMO:
+            self.codigo = TAMANHO_PADRAO_MINIMO
         else:
             self.codigo += 1
 
