f4c59d35c36b972171bfd6c583ccc38a274ea974
Arthur Carvalho <arthur.reis@live.com>
2021-08-21 19:59:36 -0300

    add injecao de dependencias

--- fila_prioritaria.py
+++ fila_prioritaria.py
@@ -1,29 +1,23 @@
-from typing import Dict, Union, List
+from typing import Union
 
 from fila_base import FilaBase
 from constantes import CODIGO_PRIORITARIO
+from estatistica_resumida import EstatisticaResumida
+from estatistica_detalhada import EstatisticaDetalhada
+
+Classes = Union[EstatisticaResumida, EstatisticaDetalhada]
 
 
 class FilaPrioritaria(FilaBase):
 
     def gera_senha_atual(self):
-        self.senha_atual = f'${CODIGO_PRIORITARIO}{self.codigo}'
+        self.senha_atual = f'{CODIGO_PRIORITARIO}{self.codigo}'
 
     def chama_cliente(self, caixa: int) -> str:
         cliente_atual = self.fila.pop(0)
         self.clientes_atendidos.append(cliente_atual)
         return (f'Cliente atual: {cliente_atual}, dirija-se ao caixa: {caixa}')
 
-    def estatistica(self, dia: str, agencia: int, flag: str) -> dict:
-        estatistica: Dict[str, Union[List[str], str, int]] = {}
-        if flag != 'detail':
-            estatistica[f'{agencia}-{dia}'] = len(self.clientes_atendidos)
-        else:
-            estatistica['dia'] = dia
-            estatistica['agencia'] = agencia
-            estatistica['clientes_atendidos'] = self.clientes_atendidos
-            estatistica['quantidade_clientes_atendidos'] = (
-                len(self.clientes_atendidos)
-            )
+    def estatistica(self, retorna_estatistica: Classes) -> dict:
 
-        return estatistica
+        return retorna_estatistica.roda_estatistica(self.clientes_atendidos)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # from fila_normal import FilaNormal
 # from fila_prioritaria import FilaPrioritaria
 from fabrica_fila import FabricaFila
+from estatistica_detalhada import EstatisticaDetalhada
+from estatistica_resumida import EstatisticaResumida
 
 # fila_teste = FilaNormal()
 # fila_teste.atualiza_fila()
@@ -17,8 +19,9 @@ from fabrica_fila import FabricaFila
 # print(fila_teste_2.chama_cliente(1))
 # print(fila_teste_2.estatistica('10/01/1993', 216, 'detail'))
 
-teste_fabrica = FabricaFila.pega_fila(1)
+teste_fabrica = FabricaFila.pega_fila('prioritaria')
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 teste_fabrica.atualiza_fila()
 print(teste_fabrica.chama_cliente(10))
+print(teste_fabrica.estatistica(EstatisticaDetalhada('20/03/2025', 120)))
